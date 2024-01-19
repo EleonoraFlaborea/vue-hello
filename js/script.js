138084c8063367d0ab3b29439406e5a1f4d2aafd
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-19 17:02:01 +0100

    add content with data

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+console.log ('Vue on', Vue)
+
+const {createApp} = Vue;
+
+const app = createApp ({
+    data(){
+        return{
+        
+            name: 'Eleonora',
+            age: 25,
+        }
+    }
+
+})
+
+app.mount('#root');
